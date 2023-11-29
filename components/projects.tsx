3bd4052587692b5c571d20481a45afb3832c07f3
Juan Ferreira <juanandres15@gmail.com>
2023-11-29 00:08:12 +0000

    fix: changes to project & projects components

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,9 +8,10 @@ import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
+
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My Projects</SectionHeading>
+      <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
